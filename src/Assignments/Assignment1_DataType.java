d88e921f426c26ac6dca1dce007516d746a90bca
Sandhya Devi R <galbedazzle@gmail.com>
2025-05-23 22:17:57 +0530

    Assignment done till 10

--- src/Assignments/Assignment1_DataType.java
+++ src/Assignments/Assignment1_DataType.java
@@ -3,7 +3,38 @@ package Assignments;
 public class Assignment1_DataType {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Temperature of a city in degrees Celsius: 25.5
+		double temperature = 25.5;
+		System.out.println(temperature);
+		//Whether a customer has placed an order: true or false
+		boolean customerorder = true;
+		System.out.println(customerorder);
+		//Person's phone number: "123-456-7890"
+		long phonenumber = 123-456-7890;
+		//Amount of money in a customer's bank account: 1000.50
+		double AmountInBankAccount = 1000.50;
+		// Person's email address: "john.doe@example.com"
+		String emailaddress = "john.doe@example.com";
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float coordinatesoflocation = 37.7749F;
+		float longitide = -122.4194F;
+		System.out.println(coordinatesoflocation);
+		System.out.println(longitide);
+		//Person's marital status: true or false
+		boolean martialStatus=true;
+		System.out.println(martialStatus);
+		//8. Person's occupation: "Software Engineer"
+		String personOccupation = "Software Engineer";
+		System.out.println(personOccupation);
+		//9. Person's favourite colour: "Blue"
+		String favouriteColour = "Blue";
+		System.out.println(favouriteColour);
+		//10.Current year: 2023
+		int currentYear = 2023;
+		System.out.println(currentYear);
+		
+		
+		
 
 	}
 
