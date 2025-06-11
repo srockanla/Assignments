98425f11332442210e1577a066fb8c74454c3169
Sandhya Devi R <galbedazzle@gmail.com>
2025-06-11 20:49:35 +0530

    Assignment 14 completed

---
+++ src/Assignments/Assignment14_powerOfaNumber.java
@@ -0,0 +1,21 @@
+package assignments;
+
+public class Assignment14_powerOfaNumber {
+
+	public static void main(String[] args) {
+		        double base = 2.10000;
+		        int exponent = 3;
+		        double result = 1;
+
+		        while(exponent >0) {
+		        	result = result * base;
+		        	exponent--;
+		        	
+		        }
+		      
+		        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+		    }
+		}
+
+	
+
